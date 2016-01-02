fb09df7c76d3437063e514f555b8982afcb932a1
Rita Mut <ritamutyaba@gmail.com>
2016-01-01 21:12:34 -0800

    cleaned up run script

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/index')
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
-app.debug = True
-app.run()
+if __name__ == '__main__':
+    app.run()
